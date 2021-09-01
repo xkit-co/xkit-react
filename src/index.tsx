83f5fc9949e34f881234c2de787dca266238612f
Nikita Kozlov <kozlovzxc@gmail.com>
2021-09-01 17:06:12 +0300

    feat(common): basic working app

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { App } from './app';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
+
