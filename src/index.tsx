488e14f8bf9b743ff3eb831bc61c4c8b213a0cd6
Nikita Kozlov <kozlovzxc@gmail.com>
2021-09-06 15:45:36 +0300

    feat(common): added prettier & fixed last line

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './app';
+import * as React from 'react'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { App } from './app'
 
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
   document.getElementById('root')
-);
-
+)
